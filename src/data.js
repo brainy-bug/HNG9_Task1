0520ed296cc210e2ca3e7b6ddbe432d3dbb832cc
maodesigns <mao.devv@gmail.com>
2022-10-29 07:49:43 +0100

    Updated subtext in links

--- src/data.js
+++ src/data.js
@@ -18,19 +18,22 @@ const links = [
   {
     id: "book__python",
     title: "Python Books",
-    subtext: "This is where you find books about design and coding",
+    subtext:
+      "The HNG Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.",
     url: "https://books.zuri.team/python-for-beginners?ref_id=AbdulrasheedMadamidola",
   },
   {
     id: "pitch",
     title: "Baskground Check for Coders",
-    subtext: "This is where you find books about design and coding",
+    subtext:
+      "The HNG Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.",
     url: "https://background.zuri.team",
   },
   {
     id: "book__design",
     title: "Design Books",
-    subtext: "This is where you find books about design and coding",
+    subtext:
+      "The HNG Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.",
     url: "https://books.zuri.team/design-rules",
   },
 ];
